0a705aba133c24ab95ac08288dd8452e3f668a0d
Matheus Brito <matheusbritodev1@gmail.com>
2025-07-04 14:04:02 -0300

    update

--- 2-Modulos/exe001.py
+++ 2-Modulos/exe001.py
@@ -4,3 +4,14 @@ Escreva um módulo em python para tratar algumas strings e que possua as seguint
 Inverter uma string de trás pra frente.
 Retornar apenas letras com índice par.
 Retornar apenas letras com índice ímpar.'''
+
+#Inverter Strings.
+import modulo001
+string = 'Matheus'
+print(f'String invertida:\n>', modulo001.inverter(string))
+
+#Retornar apenas letras com índice par.
+print(f'Retornar apenas letras com índice par:\n>', modulo001.letrasIndicePar(string))
+
+#Retornar apenas letras com índice impar.
+print(f'Retornar apenas letras com índice impar:\n>', modulo001.letrasIndiceImpar(string))
---
+++ 2-Modulos/modulo001.py
@@ -0,0 +1,13 @@
+string = 'juventude'
+
+def inverter(x):
+    x = x[::-1]
+    return x
+
+def letrasIndicePar(x):
+    x = x[1::2]
+    return x
+
+def letrasIndiceImpar(x):
+    x = x[0::2]
+    return x
