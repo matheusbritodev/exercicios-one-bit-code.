c0e7949434f36349b6755742f7dedfab70bc849f
Matheus Brito <matheusbritodev1@gmail.com>
2025-07-18 19:28:21 -0300

    update

--- 3-Poo/exe001.py
+++ 3-Poo/exe001.py
@@ -8,27 +8,56 @@ Assim que uma avaliação for realizada, deve ser incrementado o total de avalia
 
 Para cada filme ter uma nota de avaliação média que consiste na divisão do total de avaliação pelo total de avaliadores."""
 
+
 class Filme:
     def __init__(self, nome, anoDeLancamento, incluidoNoPlano, duracao ):
         self.nome = nome
         self.anoDeLancamento = anoDeLancamento
         self.incluidoNoPlano = incluidoNoPlano
         self.duracao = duracao
+        self.avaliacoes = 0
+        self.total_avaliadores = 0
 
-    def avaliacaoUsuario(self):
-        nota = float(input(f'\nO que achou de assistir "{self.nome}"? Deixe uma avaliação abaixo, onde:\n 0 = "Eu odiei o filme!"\n10 = "Eu amei o filme!"\n\nDe 0 a 10 minha nota para{self.nome} é: \n>'))
-        self.nota = nota
+    #FUNÇÃO PARA CALCULAR ASOMATIVA DAS AVALIACÕES E A QUANTIDADETOTAL DE USUÁRIOS QUE VÃO AVALIAR
+    def avaliacao_usuario(self, nota):
+        if 0 <= nota <= 10:
+            self.avaliacoes += nota #somar cada avaliação fornecida
+            self.total_avaliadores += 1 #contabilizar cada avaliação bem sucedida para descobrir o número total de usuários que avaliaram
+        else:
+            # Informa sobre o erro, mas não quebra o programa
+            print(f"Erro: A nota {nota} é inválida e não foi registrada. Por favor, use um valor entre 0 e 10.")
+        
 
+    #FUNÇÃO PARA CALCULAR A MÉDIA DAS AVALIAÇÕES
+    def media_avaliacao(self):
+        self.media = self.avaliacoes / self.total_avaliadores
+        return self.media
+
+    #FUNÇÃO PARA EXIBIR AS INFORMAÇÕES DO FILME
     def info_tec(self):
-        print(f"\n{"#" * 71}\n\n---Informações do Filme---\n")
-        print(f"Nome do filme: {self.nome}\nAno de lançamento: {self.anoDeLancamento}\nIncluído no plano: {self.incluidoNoPlano}\nAvaliação: {self.nota}\nDuração do filme em Min: {self.duracao}\n\n{"#" * 71}")
+        print(f"\n---Informações do Filme---\n")
+        print(f"Nome do filme: {self.nome}\nAno de lançamento: {self.anoDeLancamento}\nIncluído no plano: {self.incluidoNoPlano}\nAvaliação: {self.media:.1f} ({self.total_avaliadores} avaliações)\nDuração do filme em Min: {self.duracao}\n\n{"#" * 71}")
 
-#FILME 1
+#DADOS DO FILME 1
 como_treinar_seu_dragao = Filme("Como treinar o seu dragão: Live Action", 2025, True, 185)
-como_treinar_seu_dragao.avaliacaoUsuario()
-como_treinar_seu_dragao.info_tec()
 
-#FILME 2
+#avaliação dos usuários do FILME 1
+como_treinar_seu_dragao.avaliacao_usuario(9.1)
+como_treinar_seu_dragao.avaliacao_usuario(8.4)
+
+#média da avaliação dos usuários do FILME 1
+como_treinar_seu_dragao.media_avaliacao()
+
+#DADOS DO FILME 2
 madagascar = Filme("Madagascar", 2005, True, 86)
-madagascar.avaliacaoUsuario()
+
+#avaliação dos usuários do FILME 2
+madagascar.avaliacao_usuario(7.5)
+madagascar.avaliacao_usuario(9.8)
+
+#média da avaliação dos usuários do FILME 2
+madagascar.media_avaliacao()
+
+#Informações dos filmes
+como_treinar_seu_dragao.info_tec()
 madagascar.info_tec()
